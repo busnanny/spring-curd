63644911cdf9ac28d331a415f6aa035bf7d19c7a
Satish <satish@Satishs-MacBook-Air.local>
2026-04-17 18:16:05 +0530

    first commit

--- src/main/java/com/example/springboot_crud/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot_crud.repository;
-
-import com.example.springboot_crud.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
